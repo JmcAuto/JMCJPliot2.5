6e59bc309cb642c64a4acde7631b059369f66546
slquan013 <yshi11@jmc.com.cn>
2023-11-03 20:21:52 +0800

    test

--- scripts/localization_online_visualizer.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "${DIR}/.."
-
-source "${DIR}/jmc_auto_base.sh"
-
-$JMC_AUTO_BIN_PREFIX/modules/localization/msf/local_tool/local_visualization/online_visual/online_local_visualizer \
-    --flagfile=/jmcauto/modules/localization/conf/localization.conf \
-    --log_dir=/jmcauto/data/log
